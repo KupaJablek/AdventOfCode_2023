1cf8b301de8bc8a1ca4acec10bd20eff159ba237
Maximus <maximus.slabon@gmail.com>
2023-12-22 11:41:39 -0500

    added part 2 of the solution

--- 2023/05/aoc_d5_p1.py
+++ 2023/05/aoc_d5_p1.py
@@ -5,9 +5,64 @@ data = file.split("\n")
 
 seedData = data[0].split(':')
 seeds = seedData[1].strip().split(" ")
+
+seedRanges = []
+cpair = []
 for i, seed in enumerate(seeds):
+    if len(cpair) < 1:
+        cpair.append(int(seed))
+    elif len(cpair) == 1:
+        cpair.append(int(seed))
+        seedRanges.append(cpair)
+        cpair = []
+
     seeds[i] = int(seed)
 currMap = []
+
+
+def updateRangedSeeds():
+    tempSeeds = []
+    print(seedRanges)
+    for p, pair in enumerate(seedRanges):
+        seedStart = pair[0]
+        seedRange = pair[1]
+        for i, item in enumerate(currMap):
+            sourceStart, sourceRange = item[0], item[1]
+            sourceEnd = sourceStart + sourceRange - 1
+            step = item[2]
+
+            if sourceStart > seedStart and sourceStart > seedStart + seedRange:
+                # no overlap, seed range is prior to source start
+                tempSeeds.append(pair)
+                continue
+            if sourceEnd < seedStart and sourceEnd < seedStart:
+                # no overlap, seed range is post source start
+                tempSeeds.append(pair)
+                continue
+
+            # split portions of seed range that do not overlap
+            startOffset = sourceStart - seedStart
+            endOffset = (sourceEnd) - (seedStart + seedRange - 1)
+
+            if startOffset <= 0 and endOffset >= 0:
+                # everything is in range
+                tempSeeds.append([seedStart + step, seedRange])
+                continue
+
+            newSRPair = pair
+            # check for start offset
+            if startOffset > 0:
+                sr = (seedStart, startOffset)
+                tempSeeds.append(sr)
+                newSRPair = (seedStart + startOffset, seedRange - startOffset)
+            if endOffset < 0:
+                newSRPair = [newSRPair[0], newSRPair[1] + endOffset]
+                sr = [newSRPair[0] + newSRPair[1], (-1 * endOffset)]
+                tempSeeds.append(sr)
+
+            newSRPair = [newSRPair[0] + step, newSRPair[1]]
+            tempSeeds.append(newSRPair)
+    return tempSeeds
 
 
 def updateSeeds():
@@ -16,13 +71,13 @@ def updateSeeds():
             if seed >= item[0] and seed <= (item[0] + item[1]):
                 seeds[s] = seed + item[2]
                 break
-    print(seeds)
 
 
 for i, line in enumerate(data):
     if "map" in line:
         if currMap != []:
             updateSeeds()
+            seedRanges = updateRangedSeeds()
             currMap = []
     elif "seeds" not in line and line != "":
         d = line.split(" ")
@@ -37,5 +92,7 @@ if currMap != []:
     updateSeeds()
 
 seeds.sort()
-print("Final order: " + str(seeds))
+print(seedRanges)
+seedRanges = sorted(seedRanges, key=lambda e: e[0])
 print("\nLowest location #: " + str(seeds[0]))
+print("\nLowest ranged location #: " + str(seedRanges[0]))
