3a1329e4113780908e97a5d7a9a3c99637cc71ff
Maximus <maximus.slabon@gmail.com>
2024-12-05 18:56:11 -0500

    d4 finished

--- 2024/d4.c
+++ 2024/d4.c
@@ -48,55 +48,136 @@ int search(int col, int row) {
     int tot = 0;
 
     // up
-    bool found = true;
-    for (int i = 1; i < 4 && col+i < word_search_l; ++i) {
+    int i = 1;
+    for (i = 1; i < 4 && col+i < word_search_l; ++i) {
         if (word_search[col+i][row] != xmas[i]) {
-           found = false; 
            break; 
         } 
     }
 
-    if(found) {
+    if(i == 4) {
         ++tot;
     }
-    found = true;
+
     // down
-    for (int i = 1; i < 4 && col-i >= 0; ++i) {
+    for (i = 1; i < 4 && col-i >= 0; ++i) {
         if (word_search[col-i][row] != xmas[i]) {
-           found = false; 
            break; 
         } 
     }
 
-    if(found) {
+    if(i == 4) {
         ++tot;
     }
-    found = true;
+
     // left
-    for (int i = 1; i < 4 && row+i < word_search_w; ++i) {
+    for (i = 1; i < 4 && row+i < word_search_w; ++i) {
         if (word_search[col][row+i] != xmas[i]) {
-           found = false; 
            break; 
         } 
     }
 
-    if(found) {
+    if(i == 4) {
         ++tot;
     }
-    found = true;
     // right
-    for (int i = 1; i < 4 && row-i >= 0; ++i) {
+    for (i = 1; i < 4 && row-i >= 0; ++i) {
         if (word_search[col][row-i] != xmas[i]) {
-           found = false; 
            break; 
         } 
     }
 
-    if(found) {
+    if(i == 4) {
         ++tot;
     }
 
     return tot;
+}
+
+int search_diag(int col, int row) {
+    char *xmas = "XMAS";
+    int tot = 0;
+
+    // up + left
+    int i = 1;
+    for (i = 1; i < 4 && col + i < word_search_l && row + i < word_search_w; ++i) {
+        if (word_search[col + i][row + i] != xmas[i]) {
+            break;
+        }
+    }
+
+    if(i == 4) {
+        ++tot;
+    }
+
+    // down
+    for (i = 1; i < 4 && col-i >= 0 && row-i >= 0; ++i) {
+        if (word_search[col-i][row-i] != xmas[i]) {
+           break; 
+        } 
+    }
+
+    if(i == 4) {
+        ++tot;
+    }
+    // left
+    for (i = 1; i < 4 && row+i < word_search_w && col-i >= 0; ++i) {
+        if (word_search[col-i][row+i] != xmas[i]) {
+           break; 
+        } 
+    }
+
+    if(i == 4) {
+        ++tot;
+    }
+    // right
+    for (i = 1; i < 4 && row-i >= 0 && col+i < word_search_l; ++i) {
+        if (word_search[col+i][row-i] != xmas[i]) {
+           break; 
+        } 
+    }
+
+    if(i == 4) {
+        ++tot;
+    }
+
+    return tot;
+}
+
+int x_mas(int col, int row) {
+    if (col - 1 < 0 || row -1 < 0 || col+1 >= word_search_l || row+1 >= word_search_w ) {
+        return 0;
+    }
+
+    /* S M
+       S M
+    */
+    if (word_search[col+1][row+1] == 'M' && word_search[col-1][row-1] == 'S' && word_search[col-1][row+1] == 'M' && word_search[col+1][row-1] == 'S') {
+        return 1;
+    }
+
+    /* M S
+       M S
+    */
+    if (word_search[col+1][row+1] == 'S' && word_search[col-1][row-1] == 'M' && word_search[col-1][row+1] == 'S' && word_search[col+1][row-1] == 'M') {
+        return 1;
+    }
+
+    /* M M
+       S S
+    */
+    if (word_search[col+1][row+1] == 'S' && word_search[col-1][row-1] == 'M' && word_search[col-1][row+1] == 'M' && word_search[col+1][row-1] == 'S') {
+        return 1;
+    }
+
+    /* S S
+       M M
+    */
+    if (word_search[col+1][row+1] == 'M' && word_search[col-1][row-1] == 'S' && word_search[col-1][row+1] == 'S' && word_search[col+1][row-1] == 'M') {
+        return 1;
+    }
+
+    return 0;
 }
 
 int find_xmas() {
@@ -106,6 +187,7 @@ int find_xmas() {
             if(word_search[i][k] == 'X') {
                 // search
                 tot += search(i,k);
+                tot += search_diag(i,k);
             }
         }
     }
@@ -127,4 +209,15 @@ void d4() {
 
     int tot1 = find_xmas();
     printf("P1 total: %d\n", tot1);
+
+    int tot = 0;
+    for (int i = 0; i < word_search_l; ++i) {
+        for (int k = 0; k < word_search_w; ++k) {
+            if(word_search[i][k] == 'A') {
+                // search
+                tot += x_mas(i,k);
+            }
+        }
+    }
+    printf("P2 total: %d\n", tot);
 }
